5e4e1f38c6a5d2cc5ed947270a86267d7e8313c5
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-14 00:28:03 +0100

    It's Levi-o-sa, not Levio-sar
    You're a wizard Harry.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 // Richiesta all'utente di distanza da percorrere e età anagrafica
-var distance = Number(prompt('Quanti chilometri vuoi percorrere?'));
-var userAge = Number(prompt('Quanti anni hai?'));
+var distance = Number(prompt('Quanti chilometri disti dalla scuola di magia di Hogwarts?'));
+if (isNaN(distance))
+    alert("Devi inserire un numero");
+var userAge = Number(prompt('Quanti anni hai, piccolo mago?'));
+if (isNaN(userAge))
+    alert("Devi inserire un numero");
 // Costo per KM
 var costPerKm = 0.21;
 //Tariffa base
@@ -8,11 +12,19 @@ var standardCost = (distance * costPerKm);
 var totalCost;
 // Sconto 20% per età inferiore a 18, sconto 40% per età superiore a 65
 if (userAge < 18) {
-    totalCost = standardCost - (standardCost * 0.2);
+    totalCost = (standardCost * 0.8);
 } else if (userAge > 65) {
-    totalCost = standardCost - (standardCost * 0.4);
+    totalCost = (standardCost * 0.6);
 } else {
     totalCost = standardCost;
 }
 
-document.getElementById('ticketCost').innerHTML = totalCost.toFixed(2) + " €";
+if (userAge < 18) {
+    document.getElementById('reduction').innerHTML = ("Sconto del 20% applicato alla tariffa base del Ministero della Magia*");
+}
+
+if (userAge > 65) {
+    document.getElementById('reduction').innerHTML = ("Sconto del 40% applicato alla tariffa base del Ministero della Magia*");
+}
+
+document.getElementById('ticketCost').innerHTML = totalCost.toFixed(2) + " £";
